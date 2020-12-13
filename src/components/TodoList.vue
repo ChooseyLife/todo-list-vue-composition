a19d0a44df30fcb9dca454901ff52b9d55f211e4
hesj <563798627@qq.com>
2020-12-13 17:49:20 +0800

    【拆分组件】todoAdd,todoFilter,todoList,todoListItem

---
+++ src/components/TodoList.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="todo-list">
+        <todo-list-item></todo-list-item>
+    </div>
+</template>
+<script>
+import TodoListItem from './TodoListItem';
+export default {
+    components: {
+        TodoListItem
+    }
+};
+</script>
+<style>
+.todo-list {
+    display: grid;
+    row-gap: 14px;
+}
+</style>
